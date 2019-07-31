63f20a7500f0f2d18ed7c9be8d5e973922454937
Dmitriy <padavankun@gmail.com>
2019-07-31 20:05:17 +0300

    add file from pure-js boilerplate

---
+++ app/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom'
+
+const App = () => (
+  <div> Hello World </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app') as HTMLElement);
