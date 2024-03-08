56a3668a5edde631c0f70ebb61bf26fc127e96cf
Dmitrii Shikhaleev <padavankun@gmail.com>
2024-03-08 13:08:30 +0300

    types for dev build config

--- build.dev.js
+++ build.dev.js
@@ -2,6 +2,35 @@ import { context } from 'esbuild';
 import fs from 'node:fs';
 import path from 'node:path';
 
+/***
+ * @type {import("esbuild").BuildOptions}
+ **/
+const BUILD_OPTIONS = {
+  bundle: true,
+  logLevel: "info",
+  entryPoints: ["src/index.tsx"],
+  outfile: "www/app.js",
+  minify: false,
+  format: 'esm',
+  sourcemap: true,
+  loader: {
+    '.wav': 'file',
+    '.woff': 'dataurl',
+    '.svg': 'dataurl',
+  },
+  logLevel: "info"
+};
+
+/***
+ * @type {import("esbuild").ServeOptions}
+ **/
+const SERVICE_OPTIONS = {
+  servedir: 'www',
+  fallback: 'www/index.html',
+  host: "127.0.0.1",
+  port: "3000",
+}
+
 async function run() {
   const buildFolder = path.resolve('www');
   if (!fs.existsSync(buildFolder)) {
@@ -14,22 +43,9 @@ async function run() {
     { recursive: true }
   );
 
-  let ctx = await context({
-    bundle: true,
-    logLevel: "info",
-    entryPoints: ["src/index.tsx"],
-    outfile: "www/app.js",
-    loader: { '.wav': 'file' },
-    minify: false,
-    format: 'esm',
-    sourcemap: true,
-  })
-
+  let ctx = await context(BUILD_OPTIONS)
   await ctx.watch()
-
-  await ctx.serve({
-    servedir: 'www',
-  })
+  await ctx.serve(SERVICE_OPTIONS)
 }
 
 run();
